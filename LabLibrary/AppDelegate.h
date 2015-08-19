f242cadacc0c38de3cd0f9718b43594884bc0992
Cloudox <cloudox@foxmail.com>
2015-08-19 08:27:10 +0800

    Initial Commit

---
+++ LabLibrary/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LabLibrary
+//
+//  Created by csdc-iMac on 15/8/19.
+//  Copyright (c) 2015年 csdc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
